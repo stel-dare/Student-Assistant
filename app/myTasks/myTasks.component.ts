f5f66b76d7357550c68f2033a5ee4267b972a028
Stella Dare <stelladare2@gmail.com>
2018-03-22 21:33:49 +0000

    MyTAsks component up to task now...User can add task and delete task

--- app/myTasks/myTasks.component.ts
+++ app/myTasks/myTasks.component.ts
@@ -5,6 +5,8 @@ import { Color } from "color";
 //This routerExtensions is for navigating to a previous page.
 //It seems to be faster
 import { RouterExtensions  } from "nativescript-angular/router";
+import * as ApplicationSettings from "application-settings";
+const firebase = require("nativescript-plugin-firebase");
 
 import { DatePicker } from "ui/date-picker";
 import { TimePicker } from "ui/time-picker";
@@ -18,25 +20,43 @@ import { TimePicker } from "ui/time-picker";
 export class MyTasksComponent implements OnInit {
 
   collapseTask= false;
-  taskName:string="Nameless Task";
+  taskName:string;
   taskColor:string;
   colors=["#4f2729","#d2575e","#c03ea2" ,"#833ec0", "#1c3ad7","#ffc0cb", "#008080","#ffd700", "#0000ff", "#ffa500",
 "#800080", "#003366","#f6546a","#ff00ff", "#0e2f44", "#808080" ,"#daa520", "#000080", "#000080"];
 
-  //TaskArray = [{task:"eat",color:new Color("red"), date:"March"}];
+
   TaskArray=[];
 
-  chosenDate:any = "June";
+  chosenDate:any;
   chosenTime:any;
   finalDate:any;
-
+//VArriables for ngOnInit
+taskFire:any;
+finalDateFire:any;
 
 
     constructor(private router:Router ,  private routerExtensions: RouterExtensions) { }
 
+
     ngOnInit(): void {
+      //checks database to load all tasks
+      firebase.getValue( '/Tasks/'+ApplicationSettings.getString("userID"))
+         .then((result) =>{
+           console.log(JSON.stringify(result));
+
+           for (var key in result.value) {
+        this.taskFire = result.value[key].Task;
+        this.finalDateFire = result.value[key].DateOfTask + "  |  " + result.value[key].TimeOfTask;
+        this.TaskArray.push({task:this.taskFire,color:this.colors[Math.floor(Math.random() * 19)], date: this.finalDateFire, key:key});
+
+     }
+         } )
+         .catch(error => console.log("Error: " + error));
 
     }
+
+
 
 
     goBackPage(){
@@ -89,17 +109,40 @@ onTimeChanged(args) {
 
 
 addTasks(){
+  //THis function seems to do a lot of things
   this.collapseTask = false;
-
-  this.finalDate = this.chosenDate + " | " + this.chosenTime;
+  this.finalDate = this.chosenDate + "  |  " + this.chosenTime;
 
   if(this.taskName===""){
     this.taskName="Nameless Task";
   }
- this.TaskArray.push({task:this.taskName,color:new Color (this.colors[Math.floor(Math.random() * 19)]), date: this.finalDate });
-  this.taskName="";
 
-//  this.taskColor=this.colors[Math.floor( Math.random() * 5)];
+
+//It pushes to firebase before displaying
+//I used this method because I needed to store the unique key
+//There are two distinct arrays
+//One from firebase and one called Task array
+  firebase.push(
+     '/Tasks/'+ApplicationSettings.getString("userID"),
+     {Task:this.taskName, DateOfTask: this.chosenDate,  TimeOfTask:this.chosenTime}
+ ).then(
+       (result) => {
+        console.log("created key: " + result.key);
+        this.TaskArray.push({task:this.taskName,color:new Color (this.colors[Math.floor(Math.random() * 19)]), date: this.finalDate ,key:result.key});
+        console.log(JSON.stringify(this.TaskArray));
+        this.taskName="";
+      }
+  );
+
+
+
+}
+//TAkes the index from ngFOr and uses splice to remove it from the array
+//Takes unique Id from taskArray and removes it from firebase
+deleteTask(index:any,uniqueID:any){
+  this.TaskArray.splice(index,1);
+  firebase.remove('/Tasks/'+ ApplicationSettings.getString("userID") + '/' + uniqueID );
+
 }
 
 
